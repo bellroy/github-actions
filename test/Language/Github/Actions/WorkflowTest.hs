0387513ab59e1a441bb99a26f65fa7b5883166d1
Michael Webb <michaelwebb76@gmail.com>
2025-03-05 14:22:13 +1100

    Last fixes and tests passing

--- test/Language/Github/Actions/WorkflowTest.hs
+++ test/Language/Github/Actions/WorkflowTest.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Language.Github.Actions.WorkflowTest where
 
+import qualified Data.Aeson as Aeson
+import Data.Bifunctor (first)
+import qualified Data.ByteString as BS
+import Data.List (isPrefixOf)
+import qualified Data.Text as Text
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Yaml as YAML
-import Hedgehog.Extended (Property, trippingJSON)
+import Hedgehog (Gen, Property, forAll, property, tripping)
 import Language.Github.Actions.Workflow (Workflow)
 import qualified Language.Github.Actions.Workflow as Workflow
 import System.FilePath (takeBaseName)
@@ -36,21 +43,25 @@ test_goldenWorkflowFromYaml = do
               . reverse
               . drop 11
               $ reverse goldenYamlFilePath
-       in goldenVsToYamlFile
+       in goldenVsToYaml
             ("roundtrip " <> takeBaseName goldenYamlFilePath)
             goldenYamlFilePath
-            outputFilePath
             $ do
               eitherWorkflow <- YAML.decodeFileEither @Workflow goldenYamlFilePath
               either
-                (writeFileBS outputFilePath)
+                (BS.writeFile outputFilePath)
                 (writeOutputFiles outputFilePath haskellOutputFilePath)
-                $ first (encodeUtf8 . YAML.prettyPrintParseException) eitherWorkflow
+                $ first (encodeUtf8 . Text.pack . YAML.prettyPrintParseException) eitherWorkflow
     writeOutputFiles :: FilePath -> FilePath -> Workflow -> IO ()
     writeOutputFiles outputFilePath haskellOutputFilePath workflow = do
-      writeFileBS outputFilePath (YAML.encode workflow)
-        >> writeFileBS haskellOutputFilePath (encodeUtf8 $ ppShow workflow)
+      BS.writeFile outputFilePath (YAML.encode workflow)
+        >> BS.writeFile haskellOutputFilePath (encodeUtf8 . Text.pack $ ppShow workflow)
 
 hprop_WorkflowRoundTrip :: Property
 hprop_WorkflowRoundTrip =
   trippingJSON Workflow.gen
+
+trippingJSON :: (Eq a, Show a, Aeson.FromJSON a, Aeson.ToJSON a) => Gen a -> Property
+trippingJSON gen = property $ do
+  a <- forAll gen
+  tripping a Aeson.encode Aeson.eitherDecode
