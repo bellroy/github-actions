923bc3d789b9d2759fb214cd27c6eb7f578c8e9e
trike-deploy <operator@trikeapps.com>
2025-06-26 09:23:58 +1000

    Github Actions update from https://github.com/bellroy/gha-workflows/pull/743 (#2)
    * workflow update from gha-workflows
    
    * Remove support for 8.10
    
    * Restore 8.10 and try to get past the text hurdle
    
    * Try text 1.2.4
    
    * Provide another option for containers
    
    * GHC 8.10 is officially too-hard-basket
    
    * GHC 9.0 is officially too-hard-basket
    
    * Only support 9.6 onwards
    
    * Specific text versions
    
    * Try different containers versions
    
    * Add another text version
    
    * Add another bytestring version
    
    * Add another bytestring version
    
    * Add another containers version
    
    * Add another containers version
    
    * hoist-error does not support >9.8
    
    * Add golden tests files to extra-source-files
    
    * Add comment about hoist-error
    
    ---------
    
    Co-authored-by: michaelwebb76 <michaelwebb76@users.noreply.github.com>
    Co-authored-by: Michael Webb <michaelwebb76@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,18 +3,13 @@
     bellroy-nix-foss.url = "github:bellroy/bellroy-nix-foss";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.bellroy-nix-foss.lib.haskellProject {
       src = ./.;
       supportedCompilers = [
-        "ghc810"
-        "ghc90"
-        "ghc92"
-        "ghc94"
         "ghc96"
         "ghc98"
-        "ghc910"
-        "ghc912"
       ];
       defaultCompiler = "ghc96";
     };
