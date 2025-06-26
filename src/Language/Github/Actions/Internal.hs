8b13c2bb90d797f42c04ad6312809ebf145e468a
Michael Webb <michaelwebb76@gmail.com>
2025-06-26 10:06:14 +1000

    Internal module does not need examples

--- src/Language/Github/Actions/Internal.hs
+++ src/Language/Github/Actions/Internal.hs
@@ -22,35 +22,7 @@ where
 
 import qualified Data.Map as Map
 
--- | Create an inverse mapping from keys back to enumeration values.
---
--- This function takes a function that converts enumeration values to keys
--- and returns a function that can look up the original enumeration value
--- from a key. This is commonly used for parsing JSON representations of
--- enumeration types.
---
--- Example usage:
---
--- @
--- data Color = Red | Green | Blue
---  deriving (Bounded, Enum, Eq, Ord, Show)
---
--- colorToText :: Color -> Text
--- colorToText Red = "red"
--- colorToText Green = "green"
--- colorToText Blue = "blue"
---
--- textToColor :: Text -> Maybe Color
--- textToColor = inverseMap colorToText
---
--- -- Usage:
--- textToColor "red"   == Just Red
--- textToColor "green" == Just Green
--- textToColor "blue"  == Just Blue
--- textToColor "yellow" == Nothing
--- @
---
--- Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Enum.html#inverseMap>
+-- | Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Enum.html#inverseMap>
 inverseMap ::
   forall a k.
   (Bounded a, Enum a, Ord k) =>
@@ -63,20 +35,14 @@ inverseMap f = (`Map.lookup` dict)
     dict :: Map.Map k a
     dict = Map.fromList (fmapToFst f (universe @a))
 
--- | Map a function over a functor, creating tuples with the result as the first element.
---
--- Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Extra.Tuple.html#fmapToFst>
+-- | Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Extra.Tuple.html#fmapToFst>
 fmapToFst :: (Functor f) => (a -> b) -> f a -> f (b, a)
 fmapToFst = fmap . toFst
 
--- | Generate all values of a bounded enumeration type.
---
--- Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Enum.html#universe>
+-- | Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Enum.html#universe>
 universe :: (Bounded a, Enum a) => [a]
 universe = [minBound .. maxBound]
 
--- | Apply a function and create a tuple with the result as the first element.
---
--- Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Extra.Tuple.html#toFst>
+-- | Based on: <https://hackage.haskell.org/package/relude-1.2.2.0/docs/src/Relude.Extra.Tuple.html#toFst>
 toFst :: (a -> b) -> a -> (b, a)
 toFst f a = (f a, a)
