32dca66a9145fe917671a7f755acff0f0491601d
Michael Webb <michaelwebb76@gmail.com>
2025-07-21 12:40:09 +1000

    Update docs in Job.Needs

--- src/Language/Github/Actions/Job/Needs.hs
+++ src/Language/Github/Actions/Job/Needs.hs
@@ -9,12 +9,13 @@
 -- Maintainer  : Bellroy Tech Team <haskell@bellroy.com>
 --
 -- This module provides the 'JobNeeds' type for representing job dependencies
--- in GitHub Actions workflows. GitHub Actions allows both single job and
--- multiple job dependencies for the 'needs' field.
+-- in GitHub Actions workflows. GitHub Actions allows both strings and
+-- lists of strings for the 'needs' field.
 --
 -- Examples of valid 'needs' specifications:
--- * @needs: build@ - Single job dependency
--- * @needs: [build, test]@ - Multiple job dependencies
+-- * @needs: build@ - Single job specified as a string
+-- * @needs: [build]@ - Single job specified as a list of strings
+-- * @needs: [build, test]@ - Multiple job dependencies specified as a list of strings
 --
 -- For more information about GitHub Actions job dependencies, see:
 -- <https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#jobsjob_idneeds>
@@ -57,10 +58,8 @@ import qualified Language.Github.Actions.Job.Id as JobId
 -- A string input will serialize back to a string, and an array input will
 -- serialize back to an array, preventing information loss.
 data JobNeeds
-  = -- | Single job dependency as string (e.g., @needs: build@)
-    JobNeedsString JobId
-  | -- | Multiple job dependencies as array (e.g., @needs: [build, test]@)
-    JobNeedsArray (NonEmpty JobId)
+  = JobNeedsString JobId
+  | JobNeedsArray (NonEmpty JobId)
   deriving stock (Eq, Generic, Ord, Show)
 
 instance FromJSON JobNeeds where
@@ -71,7 +70,6 @@ instance ToJSON JobNeeds where
   toJSON (JobNeedsString jobId) = toJSON jobId
   toJSON (JobNeedsArray jobIds) = toJSON jobIds
 
--- | Generate random 'JobNeeds' values for property testing.
 gen :: (MonadGen m) => m JobNeeds
 gen =
   Gen.choice
