6b28e09c26a49f58091c6c8cf4c81c6e7a9b4ac5
Michael Webb <michaelwebb76@gmail.com>
2025-08-19 15:49:31 +1000

    Remove the bytestring dependency

--- test/Language/Github/Actions/WorkflowTest.hs
+++ test/Language/Github/Actions/WorkflowTest.hs
@@ -6,10 +6,9 @@ module Language.Github.Actions.WorkflowTest where
 
 import qualified Data.Aeson as Aeson
 import Data.Bifunctor (first)
-import qualified Data.ByteString as BS
 import Data.List (isPrefixOf)
 import qualified Data.Text as Text
-import Data.Text.Encoding (encodeUtf8)
+import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Yaml as Yaml
 import Hedgehog (Gen, Property, forAll, property, tripping)
 import Language.Github.Actions.Workflow (Workflow)
@@ -50,13 +49,13 @@ test_goldenWorkflowFromYaml = do
               putStrLn $ "roundtrip " <> takeBaseName testYamlFilePath
               eitherWorkflow <- Yaml.decodeFileEither @Workflow testYamlFilePath
               either
-                (BS.writeFile outputFilePath >> (\e -> fail $ "YAML decoding failed: " ++ show e))
+                (writeFile outputFilePath >> (\e -> fail $ "YAML decoding failed: " ++ show e))
                 (\workflow -> writeOutputFiles outputFilePath haskellOutputFilePath workflow >> pure workflow)
-                $ first (encodeUtf8 . Text.pack . Yaml.prettyPrintParseException) eitherWorkflow
+                $ first Yaml.prettyPrintParseException eitherWorkflow
     writeOutputFiles :: FilePath -> FilePath -> Workflow -> IO ()
     writeOutputFiles outputFilePath haskellOutputFilePath workflow = do
-      BS.writeFile outputFilePath (Yaml.encode workflow)
-        >> BS.writeFile haskellOutputFilePath (encodeUtf8 . Text.pack $ ppShow workflow)
+      writeFile outputFilePath (Text.unpack . decodeUtf8 $ Yaml.encode workflow)
+        >> writeFile haskellOutputFilePath (ppShow workflow)
 
 hprop_WorkflowRoundTrip :: Property
 hprop_WorkflowRoundTrip =
